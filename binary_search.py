ca34f6f6887414ef82f12d9129094bc1e256df82
Admin <brycealvir@gmail.com>
2022-10-02 10:19:09 -0700

    added binary algo

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,24 @@
-# code your iterative algorithm here
+# We are going to write a binary search algorithm. If the desired element is found, we want the function to return True. If the desired element is not found, we want the function to return False.
 
+          
+def binary_search(data, el):
+    first = 0
+    last = len(data)-1 
+    found = False
+
+    while first <= last and not found:
+        mid = (first+last)//2
+
+        if data[mid] == el:
+            found = True
+        else:
+            if el < data[mid]: 
+                last = mid-1
+            else: 
+                first = mid+1
+    return found
+
+test_list = [5, 8, 12, 14, 19, 22, 27, 30, 31]
+
+print(binary_search(test_list, 12))
+      
